541523f41ef010c9b2e1eb876df272bc415a322c
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-04 14:46:49 +0700

    data mhs tersebut

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -11,3 +11,19 @@ public:
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim();    // Member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2;          // Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // pointer Dereference pMhs
+    pMhs->nim = 3;          // Arrow Operator
+    pMhs->showNim();
+
+    return 0;
+}
