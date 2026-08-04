5f9fdfac2fe434bc54acf66b33f73b1875fc3d71
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-04 14:40:55 +0700

    data mahaiswa/pointer obyek mhs

--- pointerdua.cpp
+++ pointerdua.cpp
@@ -11,3 +11,15 @@ public:
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1}; // pointer Object mhs
+
+    mhs->nim = 2;
+    mhs->showNim();
+
+    delete mhs;
+
+    return 0;
+}
