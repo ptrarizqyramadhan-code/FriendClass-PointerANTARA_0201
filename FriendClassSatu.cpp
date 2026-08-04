6131ede0d914640b6a2f96c77b19482a7bd733b9
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-04 10:15:50 +0700

    class siswa bisa ngeakses class nama

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -12,3 +12,18 @@ public:
     void setNama(string pNama);
     friend class siswa;
 };
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
